06a5da8510fad91e44cdfb4014eca20dfe25b261
Twahidul Islam <twahidulislam2005@gmail.com>
2025-05-22 01:37:58 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const counterUp = document.querySelector(".counterUp");
+console.log(counterUp.dataset.number);
+
+
+// let count = 0;
+// function counterJs() {
+//   count++;
+//   counterUp.innerHTML = count;
+//   if (count == 10) {
+//     clearInterval(stop)
+//   }
+// }
+
+// let stop = setInterval(() => {
+//   counterJs();
+// }, 1000);
