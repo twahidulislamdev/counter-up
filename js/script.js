c691ea1a277e89cf727509a1c3f24e2a7116dad5
Twahidul Islam <twahidulislam2005@gmail.com>
2025-05-22 12:35:03 +0600

    add some thing

--- js/script.js
+++ js/script.js
@@ -1,16 +1,34 @@
-const counterUp = document.querySelector(".counterUp");
-console.log(counterUp.dataset.number);
+let counterUp = document.querySelectorAll(".counterUp")
+let arr = Array.from(counterUp)
+arr.map((items)=>{
+    let count = 0
+    function counterJs(){
+        count++
+        items.innerHTML = count
+        if(count == items.dataset.number){
+            clearInterval(stop)
+        }
+    }
 
+    let stop = setInterval(function(){
+        counterJs()
+    },5500/items.dataset.number)
+})
 
-// let count = 0;
-// function counterJs() {
-//   count++;
-//   counterUp.innerHTML = count;
-//   if (count == 10) {
-//     clearInterval(stop)
+// let counterUp = document.querySelectorAll(".counterUp");
+// let arr = Array.from(counterUp);
+// // console.log(arr)
+
+// arr.map((item) => {
+//   let counter = 0;
+//   function count() {
+//     counter++;
+//     item.innerHTML = counter;
+//     if (counter == item.dataset.number) {
+//       clearInterval(stop);
+//     }
 //   }
-// }
-
-// let stop = setInterval(() => {
-//   counterJs();
-// }, 1000);
+//   let stop = setInterval(function () {
+//     count();
+//   },8000/ item.dataset.number);
+// });
